3632bb49cfa95b52b5231dcd3ab4a4eed7d9216b
Peter Buckley <8352591+pbuckley@users.noreply.github.com>
2025-03-28 10:01:19 -0400

    latest state of the art and still more I want to do

--- .buildkite/scripts/get_releases.py
+++ .buildkite/scripts/get_releases.py
@@ -13,21 +13,10 @@ def main():
     service_pipelines = ["service-foo", "service-bar", "service-web"]
 
 # on a hosted agent
-    api_token = subprocess.run(['buildkite-agent', 'secret', 'get', 'readtokenpb'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-
-# get_api_token= subprocess.run(['buildkite-agent', 'secret', 'get', 'readtokenpb'],
-#                        capture_output=True, 
-#                        text=True)
-
-    # get_api_token = subprocess.run(['echo', '${BK_API_TOKEN}'],
-    #                               capture_output=True,
-    #                               text=True)
-
-    # api_token = get_api_token.stdout
-
+    # api_token = subprocess.run(['buildkite-agent', 'secret', 'get', 'readtokenpb'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     ## local run
-    # api_token = environ["BK_API_TOKEN"]
+    api_token = environ["BK_API_TOKEN"]
     # print(api_token)
 
     # headers = {'Authorization: Bearer': api_token}
@@ -41,6 +30,23 @@ def main():
     svc_builds_json = json.loads(svc_builds.decode('utf-8'))
     print(json.dumps(svc_builds_json))
 
+    # get one build
+
+    for svc_build in svc_builds_json:
+        print(svc_build['url'])
+        one_build_request = Request(svc_build['url'])
+        one_build_request.add_header('Authorization', "Bearer " + api_token)
+
+        one_build_result = urlopen(build_request).read()
+
+        one_build_json = json.loads(one_build_result.decode('utf-8'))
+        # print(json.dumps(one_build_json))
+        for meta_data in one_build_json:
+            print(meta_data['meta_data']['rel-ver'])
+
+# came so far and got so close but we just have the meta_data and not the yaml to generate it :(
+
+    # https://api.buildkite.com/v2/organizations/{org.slug}/pipelines/{pipeline.slug}/builds/{number}"
 
 if __name__ == "__main__":
     main()
