1544b1e554774018a3db5e675f74e02006c628e4
Peter Buckley <8352591+pbuckley@users.noreply.github.com>
2025-04-01 11:24:31 -0400

    more refactoring in prep for deploy-hosts and deploy-post-script

--- .buildkite/scripts/get_releases.py
+++ .buildkite/scripts/get_releases.py
@@ -40,14 +40,41 @@ def get_tagged_pipelines(api_token, org_name, given_tag):
     return filtered_list
 
 
-def get_release_versions(api_token, org_name, deployable_service_pipelines):
+def get_release_versions_from_metadata(svc_name, full_metadata, svc_dict, svc_build_versions):
     release_version_md_name = "rel-ver" # I want to prefix with deploy- eventually so deploy-rel-ver
+
+    print(f"Full metadata for {svc_name}: {full_metadata}")
+    release_version = full_metadata[release_version_md_name]
+    print(f"Found a {svc_name} release version: {release_version}")
+    svc_build_versions.add(release_version)
+    svc_dict[svc_name] = sorted(svc_build_versions)
+
+    return svc_dict
+
+
+def get_release_versions(api_token, org_name, deployable_service_pipelines):
+    # currently, this fn is tailored to release version
+    # what if we generic-ified it, made it just grab the metadata from all the builds
+    # then we return that metadata, and we can take our 3 (or more)
+    # passes over it to translate_service_versions_to_bk_yaml (and hosts, pwsh, etc)
+    # given that this fn collapses rel-ver into a set so we don't have dupes,
+    # would we do the same with the generic version? or would the translation deal with dupes
+    # each in their own way for ver/host/pwsh?
+    # maybe this fn becomes as simple as "get the metadata for all the builds" (a list)
+    # and the 3 translate fns can act on it as they like, dealing with whatever complexity
+    # and differentiation that they need to specific to their demesnes (again ver/host/pwsh)
+    # I think I like this approach of "get_metadata" and dispatch to each type (and their corresponding
+    # complexity) better - that way the get_metadata stays the same no matter how much
+    # we add or change, and the individual translate fns will handle their demesnes
+
+    # ok, all well and good sounding, but the problem is we need to iterate over
+    # multiple builds to collect metadata from them all
+    # so we need a collection/collector (list?) for the metadata
+    # but right now we're distilling into a dict of the {service-name: [<release versions>], ...}
 
     svc_dict = {}
     for pipeline_details in deployable_service_pipelines:
-        named_pipeline = pipeline_details['name']
-        print(f"Getting builds for {pipeline_details['name']}...")
-        build_request = Request(f"https://api.buildkite.com/v2/organizations/{org_name}/pipelines/{named_pipeline}/builds")
+        build_request = Request(pipeline_details['url'] + "/builds")
         build_request.add_header('Authorization', "Bearer " + api_token)
 
         svc_builds = urlopen(build_request).read()
@@ -64,23 +91,10 @@ def get_release_versions(api_token, org_name, deployable_service_pipelines):
 
             one_build_result = urlopen(build_request).read()
             one_build_json = json.loads(one_build_result.decode('utf-8'))
-            # print(json.dumps(one_build_json))
 
-            # {'select': 'Service Foo version', 'key': 'svc-foo-ver', 'options': [{'label': '1.33', 'value': '1.33'}, {'label': '1.35', 'value': '1.35'}, {'label': '1.38', 'value': '1.38'}]}
-            # template_dict = {'select': 'Service Foo version', 'key': 'svc-foo-ver', 'options': [{'label': '1.33', 'value': '1.33'}, {'label': '1.35', 'value': '1.35'}, {'label': '1.38', 'value': '1.38'}]}
-            # so we reconstruct ^ this and populate with our name and release versions?
-            # and we do it for multiples... then we need to inject into our base_pipeline somehow?
-            # svc_dict = template_dict
-            # svc_dict['select'] = svc_name + " version"
-            # svc_dict['key'] = svc_name + "-ver" # need to control for key character restrictions
             svc_build_versions = set()
-
             for build_details in one_build_json:
-                print(f"Full metadata for {svc_name}: {build_details['meta_data']}")
-                release_version = build_details['meta_data'][release_version_md_name]
-                print(f"Found a {svc_name} release version: {release_version}")
-                svc_build_versions.add(release_version)
-            svc_dict[svc_name] = sorted(svc_build_versions)
+                svc_dict = get_release_versions_from_metadata(svc_name, build_details['meta_data'], svc_dict, svc_build_versions)
 
     return svc_dict
 
@@ -94,10 +108,11 @@ def translate_service_versions_to_bk_yaml(all_services_and_versions):
     # we need to take "everything" for each of our fields - one for each service -
     # and iterate over them here
     for k, v in all_services_and_versions.items():
+        keysafe_k = k.replace(" ", "-").lower()
         option_list = []
         for ver in v:
             option_list.append({"label": ver, "value": ver})
-        generated_svc_yaml = {"select": f"{k} version", "key": f"{k}-ver", "options": option_list}
+        generated_svc_yaml = {"select": f"{k} version", "key": f"{keysafe_k}-ver", "options": option_list}
         print(generated_svc_yaml)
 
 
@@ -112,8 +127,9 @@ def translate_service_hosts_to_bk_yaml(all_services_and_hosts):
     # I want to keep the emoji - do I make that metadata, too, or can I go with the pipeline's emoji
     # and grab from one of our existing API calls to reuse it here?
     for k, v in all_services_and_hosts.items():
+        keysafe_k = k.replace(" ", "-").lower()
         option_list = [] # can be a scalar string as csv I bet, no need for a list here
-        generated_svc_yaml = {"text": f"{k} host list", "key": f"{k}-hosts", "default": option_list, "hint": f"Comma separated list of hosts to deploy {k} onto", "required": True,}
+        generated_svc_yaml = {"text": f"{k} host list", "key": f"{keysafe_k}-hosts", "default": option_list, "hint": f"Comma separated list of hosts to deploy {k} onto", "required": True,}
         print(generated_svc_yaml)
 
 
@@ -126,8 +142,9 @@ def translate_service_pwsh_to_bk_yaml(all_services_and_pwsh):
     # and iterate over them here
     example_dict = {"text": ":pwsh: Service Foo post config script", "key": "svc-foo-pwsh", "hint": "Provide filename for optional PS1 to run post deploy.", "required": False, "default": "ServiceFooDefault.PS1"}
     for k, v in all_services_and_pwsh.items():
+        keysafe_k = k.replace(" ", "-").lower()
         option_list = [] # can be a scalar string as csv I bet, no need for a list here
-        generated_svc_yaml = {"text": f"{k} post config script", "key": f"{k}-pwsh", "default": option_list, "hint": "Provide filename for optional PS1 to run post deploy.", "required": False,}
+        generated_svc_yaml = {"text": f"{k} post config script", "key": f"{keysafe_k}-pwsh", "default": option_list, "hint": "Provide filename for optional PS1 to run post deploy.", "required": False,}
         print(generated_svc_yaml)
 
 
