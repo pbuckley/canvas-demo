9cf908a0c247313e4204f51267e48115291a462f
Peter Buckley <8352591+pbuckley@users.noreply.github.com>
2025-04-09 17:13:56 -0400

    refactored bash -> python confirmed working locally

--- .buildkite/scripts/generate_deploy_targets.py
+++ .buildkite/scripts/generate_deploy_targets.py
@@ -1,145 +1,110 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python
 
-IFS=","
+from os import getenv, popen, system
+import datetime
+from benedict import benedict
+
 
 # so maybe our initial dynamic pipeline could create an artifact file (.json or somesuch)
 # that contains all the metadata keys that we're using, and we can iterate over
 # them here, which maybe would be better done with python than this simple bash
 
-export SVC_FOO_VER=$(buildkite-agent meta-data get "foo-app-ver")
-export SVC_BAR_VER=$(buildkite-agent meta-data get "bar-service-ver")
-export SVC_WEB_VER=$(buildkite-agent meta-data get "service-web-ver")
-export SVC_FOO_HOSTS=$(buildkite-agent meta-data get "foo-app-hosts")
-export SVC_BAR_HOSTS=$(buildkite-agent meta-data get "bar-service-hosts")
-export SVC_WEB_HOSTS=$(buildkite-agent meta-data get "service-web-hosts")
-export SVC_FOO_PWSH=$(buildkite-agent meta-data get "foo-app-pwsh")
-export SVC_BAR_PWSH=$(buildkite-agent meta-data get "bar-service-pwsh")
-export SVC_WEB_PWSH=$(buildkite-agent meta-data get "service-web-pwsh")
+
+def create_dynamic_step_key(prefix_fragment):
+    '''
+    create a dynamic step key so this dynamic pipeline generator
+    can be run multiple times in the same pipeline
+    we only need to go to the minute, seconds would be overkill?
+    '''
+    now = datetime.datetime.now()
+    return prefix_fragment + '-' + now.strftime("%Y-%m-%d-%H-%M")
 
 
-export DATE_STRING=$(date "+%Y-%m-%d-%H-%M")
+def main():
 
-export FOO_PRE=$(cat <<FOOPRE
-steps:
-  - group: ":rocket: :windows: Service Foo Parallel Deploys"
-    key: "foo-deploys-${DATE_STRING}"
-    steps:
-FOOPRE
-)
+    if getenv("BUILDKITE_COMPUTE_TYPE") is not None:
+        # ver is scalar
+        svc_foo_ver = str.strip(popen("buildkite-agent meta-data get foo-app-ver").read())
+        svc_bar_ver = str.strip(popen("buildkite-agent meta-data get bar-service-ver").read())
+        svc_web_ver = str.strip(popen("buildkite-agent meta-data get service-web-ver").read())
 
-export BAR_PRE=$(cat <<BARPRE
-  - group: ":rocket: :windows: Service Bar Parallel Deploys"
-    key: "bar-deploys-${DATE_STRING}"
-    steps:
-BARPRE
-)
+        # hosts is csv
+        svc_foo_hosts = str.strip(popen("buildkite-agent meta-data get foo-app-hosts").read()).split(",")
+        svc_bar_hosts = str.strip(popen("buildkite-agent meta-data get bar-service-hosts").read()).split(",")
+        svc_web_hosts = str.strip(popen("buildkite-agent meta-data get service-web-hosts").read()).split(",")
 
-export WEB_PRE=$(cat <<WEBPRE
-  - group: ":rocket: :windows: Service Web Parallel Deploys"
-    key: "web-deploys-${DATE_STRING}"
-    steps:
-WEBPRE
-)
+        # scripts is csv, too? but I don't like that it is :(
+        svc_foo_pwsh = str.strip(popen("buildkite-agent meta-data get foo-app-pwsh").read()).split(",")
+        svc_bar_pwsh = str.strip(popen("buildkite-agent meta-data get bar-service-pwsh").read()).split(",")
+        svc_web_pwsh = str.strip(popen("buildkite-agent meta-data get service-web-pwsh").read()).split(",")
+    else:
+        # ver is scalar
+        svc_foo_ver = '1.234'
+        svc_bar_ver = '4.56'
+        svc_web_ver = '7.89a'
 
-export ROLLBACK=$(cat <<ROLLY
-  - block: "Rollback / Redeploy ?"
-    key: "rollback-block-${DATE_STRING}"
-  - label: ':rewind: Rollback / Redeploy'
-    key: "rollback-redeploy-dynamic-${DATE_STRING}"
-    command: 'python .buildkite/scripts/get_releases.py'
-    depends_on:
-      - "rollback-block-${DATE_STRING}"
-ROLLY
-)
+        # hosts is csv
+        svc_foo_hosts = 'appsrv01,appsrv02,appsrv03'.split(",")
+        svc_bar_hosts = 'filesrv06,filesrv18'.split(",")
+        svc_web_hosts = 'websrv10,websrv13,websrv15'.split(",")
 
-export ALL_POST=$(cat <<ALLPOST
-queue: "q1"
-ALLPOST
-)
+        # scripts is csv, too? but I don't like that it is :(
+        svc_foo_pwsh = 'FooAppScript.PS1'
+        svc_bar_pwsh = 'BarPostDeploy.sh'
+        svc_web_pwsh = 'webconfig.py'
 
-echo "+++ SVC ENV VARS"
-env | grep SVC
+    foo_step_key = create_dynamic_step_key('foo-deploys')
+    bar_step_key = create_dynamic_step_key('bar-deploys')
+    web_step_key = create_dynamic_step_key('web-deploys')
+    rollback_block_key = create_dynamic_step_key('rollback-block')
+    rollback_redeploy_key = create_dynamic_step_key('rollback-redeploy-dynamic')
 
-# this is it, the loops
+    foo_prefix = [{'group': ':rocket: :windows: Service Foo Parallel Deploys', 'key': foo_step_key, 'steps': []}]
+    bar_prefix = [{'group': ':rocket: :windows: Service Bar Parallel Deploys', 'key': bar_step_key, 'steps': []}]
+    web_prefix = [{'group': ':rocket: :windows: Service Web Parallel Deploys', 'key': web_step_key, 'steps': []}]
 
-printf "%s\n" "$FOO_PRE" > newly_genned_pipeline.yml
+    rollback_snippet = [{'block': "Rollback / Redeploy ?", 'key': rollback_block_key}, {'label': ':rewind: Rollback / Redeploy', 'key': rollback_redeploy_key, 'command': 'python .buildkite/scripts/get_releases.py', 'depends_on': [rollback_block_key]}]
 
-echo "+++ With just the FOO_PRE"
-cat newly_genned_pipeline.yml
+    print("All svc vars")
+    print(f"svc_foo_ver: {svc_foo_ver}")
+    print(f"svc_bar_ver: {svc_bar_ver}")
+    print(f"svc_web_ver: {svc_web_ver}")
+    print(f"svc_foo_hosts: {svc_foo_hosts}")
+    print(f"svc_bar_hosts: {svc_bar_hosts}")
+    print(f"svc_web_hosts: {svc_web_hosts}")
+    print(f"svc_foo_pwsh: {svc_foo_pwsh}")
+    print(f"svc_bar_pwsh: {svc_bar_pwsh}")
+    print(f"svc_web_pwsh: {svc_web_pwsh}")
 
-read -ra FOO_HOSTS <<< "${SVC_FOO_HOSTS}"
+    full_foo_hosts = []
+    for foo_host in svc_foo_hosts:
+        full_foo_hosts.insert(0, {'label': f':windows: Deploy Service Foo {svc_foo_ver} to {foo_host}', 'command': '.buildkite/scripts/run_mock_deploy.sh', 'retry': {'automatic': [{'exit_status': '*', 'limit': '10'}]}})
+        full_foo_hosts.insert(1, {'label': f':pwsh: Run {svc_foo_pwsh} for Foo on {foo_host}', 'command': f'echo Running {svc_foo_pwsh} on {foo_host}...'})
 
-for FOO_HOST in "${FOO_HOSTS[@]}"
-do
-    echo "creating step for ${FOO_HOST} with ${SVC_FOO_VER} and ${SVC_FOO_PWSH}"
-    export FOO_BODY=$(cat <<FOOBOD
-      - label: ":windows: Deploy Service Foo ${SVC_FOO_VER} to ${FOO_HOST}"
-        command: ".buildkite/scripts/run_mock_deploy.sh"
-        retry:
-          automatic:
-            - exit_status: "*"
-              limit: 10
-      - label: ":pwsh: Run ${SVC_FOO_PWSH} for Foo on ${FOO_HOST}"
-        command: "echo Running ${SVC_FOO_PWSH} on ${FOO_HOST}..."
-FOOBOD
-)
-    echo ${FOO_BODY} >> newly_genned_pipeline.yml
-done
+    full_bar_hosts = []
+    for bar_host in svc_bar_hosts:
+        full_bar_hosts.insert(0, {'label': f':windows: Deploy Service Bar {svc_bar_ver} to {bar_host}', 'command': '.buildkite/scripts/run_mock_deploy.sh', 'retry': {'automatic': [{'exit_status': '*', 'limit': '10'}]}})
+        full_bar_hosts.insert(1, {'label': f':pwsh: Run {svc_bar_pwsh} for Bar on {bar_host}', 'command': f'echo Running {svc_bar_pwsh} on {bar_host}...'})
 
-echo "+++ NOW WITH FOOBOD"
-cat newly_genned_pipeline.yml
+    full_web_hosts = []
+    for web_host in svc_web_hosts:
+        full_web_hosts.insert(0, {'label': f':windows: Deploy Service Web {svc_web_ver} to {web_host}', 'command': '.buildkite/scripts/run_mock_deploy.sh', 'retry': {'automatic': [{'exit_status': '*', 'limit': '10'}]}})
+        full_web_hosts.insert(1, {'label': f':pwsh: Run {svc_web_pwsh} for Web on {web_host}', 'command': f'echo Running {svc_web_pwsh} on {web_host}...'})
 
-echo ${BAR_PRE} >> newly_genned_pipeline.yml
+    foo_prefix[0]['steps'] = full_foo_hosts
+    bar_prefix[0]['steps'] = full_bar_hosts
+    web_prefix[0]['steps'] = full_web_hosts
 
-read -ra BAR_HOSTS <<< "${SVC_BAR_HOSTS}"
+    full_pipeline = benedict({'steps': foo_prefix + bar_prefix + web_prefix + rollback_snippet, 'queue': 'q1'})
 
-for BAR_HOST in "${BAR_HOSTS[@]}"
-do
-    echo "creating step for ${BAR_HOST} with ${SVC_BAR_VER} and ${SVC_BAR_PWSH}"
-    export BAR_BODY=$(cat <<BARBOD
-      - label: ":windows: Deploy Service Bar ${SVC_BAR_VER} to ${BAR_HOST}"
-        command: ".buildkite/scripts/run_mock_deploy.sh"
-        retry:
-          automatic:
-            - exit_status: "*"
-              limit: 10
-      - label: ":pwsh: Run ${SVC_BAR_PWSH} for Bar on ${BAR_HOST}"
-        command: "echo Running ${SVC_BAR_PWSH} on ${BAR_HOST}..."
-BARBOD
-)
-    echo ${BAR_BODY} >> newly_genned_pipeline.yml
-done
+    full_pipeline.to_yaml(filepath='newly_genned_pipeline.yml')
 
-echo ${WEB_PRE} >> newly_genned_pipeline.yml
+    if getenv("BUILDKITE_COMPUTE_TYPE") is not None:
+        print("In hosted env, uploading pipeline")
+        system('buildkite-agent pipeline upload newly_genned_pipeline.yml')
+    else:
+        print("Running locally, would have run: buildkite-agent pipeline upload newly_genned_pipeline.yml")
 
-read -ra WEB_HOSTS <<< "${SVC_WEB_HOSTS}"
 
-for WEB_HOST in "${WEB_HOSTS[@]}"
-do
-    echo "creating step for ${WEB_HOST} with ${SVC_WEB_VER} and ${SVC_WEB_PWSH}"
-    export WEB_BODY=$(cat <<WEBBOD
-      - label: ":windows: Deploy Service Web ${SVC_WEB_VER} to ${WEB_HOST}"
-        command: ".buildkite/scripts/run_mock_deploy.sh"
-        retry:
-          automatic:
-            - exit_status: "*"
-              limit: 10
-      - label: ":pwsh: Run ${SVC_WEB_PWSH} for Web on ${WEB_HOST}"
-        command: "echo Running ${SVC_WEB_PWSH} on ${WEB_HOST}..."
-WEBBOD
-)
-    echo ${WEB_BODY} >> newly_genned_pipeline.yml
-done
-
-echo ${ROLLBACK} >> newly_genned_pipeline.yml
-
-echo ${ALL_POST} >> newly_genned_pipeline.yml
-
-echo "+++ Now with ALLPOST"
-cat newly_genned_pipeline.yml
-
-buildkite-agent artifact upload newly_genned_pipeline.yml
-
-buildkite-agent pipeline upload newly_genned_pipeline.yml
-
-rm -f newly_genned_pipeline.yml
+if __name__ == "__main__":
+    main()
